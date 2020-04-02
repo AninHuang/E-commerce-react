995ca60b1ce046ca9883b65ec28d63947ecccdd2
AninHuang <lifeanne19904@gmail.com>
2020-04-02 13:16:55 +0800

    Routing in the project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React, { Component } from 'react';
-import './App.css';
-import HomePage from './homepage.component';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+import HomePage from "./homepage.component";
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage}></Route>
+        <Route path="/hats" component={HatsPage}></Route>
+      </Switch>
     </div>
-  )
+  );
 }
 
 export default App;
