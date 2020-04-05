3e15f80f7d6707e9e7a61b2e371760f8da33a449
AninHuang <lifeanne19904@gmail.com>
2020-04-05 12:49:29 +0800

    React Router Dom

--- src/App.js
+++ src/App.js
@@ -3,18 +3,33 @@ import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import HomePage from "./homepage.component";
 
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-);
+const TopicsList = props => {
+  console.log(props); // { history: {...}, location: {...}, match: {...} }
+
+  return (
+    <div>
+      <h1>TopicsList</h1>
+    </div>
+  );
+};
+
+const TopicDetail = props => {
+  console.log(props);
+
+  return (
+    <div>
+      <h1>TopicDetail</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div>
       <Switch>
-        <Route exact path="/" component={HomePage}></Route>
-        <Route path="/hats" component={HatsPage}></Route>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/topics" component={TopicsList} />
+        <Route path="/topics/:topicId" component={TopicDetail} />
       </Switch>
     </div>
   );
