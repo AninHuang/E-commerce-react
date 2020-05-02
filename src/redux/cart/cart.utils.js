44c8067daf19779c93830eeb610fc4ec40f3f225
AninHuang <lifeanne19904@gmail.com>
2020-05-02 15:19:55 +0800

    Adding Multiple Items To Cart and removing duplicate

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,17 @@
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+  // 確認新增的是否為已存在之項目
+  const existingCartItem = cartItems.find(
+    cartItem => cartItems.id === cartItemToAdd.id
+  );
+
+  if (existingCartItem) {
+    return cartItems.map(cartItem =>
+      cartItems.id === cartItemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+
+  // 若是第一次新增項目，則附加一屬性 quantity
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
