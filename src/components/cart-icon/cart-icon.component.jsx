bd359f13e2664e886d26ae6b8a58003c07771af8
AninHuang <lifeanne19904@gmail.com>
2020-05-02 17:42:55 +0800

    Implement reselect library in order to memoize and not re-render a component if the state value does not change

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { toggleCartDropdownHidden } from "../../redux/cart/cart.actions";
+import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
 
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import "./cart-icon.styles.scss";
@@ -12,12 +13,24 @@ const CartIcon = ({ toggleCartDropdownHidden, itemCount }) => (
   </div>
 );
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  itemCount: cartItems.reduce(
-    (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity,
-    0
-  )
+const mapStateToProps = state => ({
+  itemCount: selectCartItemsCount(state)
 });
+
+// const mapStateToProps = ({ cart: { cartItems } }) => {
+//   console.log("Being called!"); // state 永遠都是新的物件，會一直觸發這裡的呼叫
+//   // 解決方法：儲存或快取
+//   // Memorization: the cacheing of the selectors value
+//   // 借助函式庫 reselect，判斷 state 物件是否真的有改變才重新渲染
+//   // to memoize and not re-render a component if the state value does not change
+//   return {
+//     itemCount: cartItems.reduce(
+//       (accumulatedQuantity, cartItem) =>
+//         accumulatedQuantity + cartItem.quantity,
+//       0
+//     )
+//   };
+// };
 
 const mapDispatchToProps = dispatch => ({
   toggleCartDropdownHidden: () => dispatch(toggleCartDropdownHidden())
---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,19 @@
+import { createSelector } from "reselect";
+
+// A function that gets the whole state and just returns a slice of it
+const selectCart = state => state.cart;
+
+export const selectCartItems = createSelector(
+  [selectCart],
+  cart => cart.cartItems
+);
+
+export const selectCartItemsCount = createSelector(
+  [selectCartItems],
+  cartItems =>
+    cartItems.reduce(
+      (accumulatedQuantity, cartItem) =>
+        accumulatedQuantity + cartItem.quantity,
+      0
+    )
+);
