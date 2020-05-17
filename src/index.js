de14561cfb976f54a9d56c44a32f1f8ba99a9707
AninHuang <lifeanne19904@gmail.com>
2020-05-17 23:44:20 +0800

    Wrap root component with PersistGate

--- src/index.js
+++ src/index.js
@@ -2,16 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+// PersistGate delays the rendering of your app's UI
+// until your persisted state has been retrieved and saved to redux.
 
 import "./index.css";
 import App from "./App";
 
-import store from "./redux/store";
+import { store, persistor } from "./redux/store";
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById("root")
--- src/redux/store.js
+++ src/redux/store.js
@@ -6,11 +6,9 @@ import rootReducer from "./root-reducer";
 
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 // If there's only one middleware
 // const store = createStore(rootReducer, applyMiddleware(logger));
 
-const persistor = persistStore(store);
-
-export default { store, persistor };
+export const persistor = persistStore(store);
