7b0b1dfa2718054470ed5223a319ac3d8f5b6dfb
AninHuang <lifeanne19904@gmail.com>
2020-05-26 23:51:00 +0800

    Nested routing

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,12 +1,19 @@
 import React from "react";
+import { Route } from "react-router-dom";
 
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
+import CategoryPage from "../category/category.component";
 
-const ShopPage = ({ match }) => (
-  <div className="shop-page">
-    <CollectionsOverview />
-  </div>
-);
+const ShopPage = ({ match }) => {
+  console.log(match); // { path: "/shop", url: "/shop", isExact: true, params: {} }
+
+  return (
+    <div className="shop-page">
+      <Route exact path={`${match.path}`} component={CollectionsOverview} />
+      <Route path={`${match.path}/:categoryId`} component={CategoryPage} />
+    </div>
+  );
+};
 
 export default ShopPage;
 // ShopPage is a simple non-connected component now.
