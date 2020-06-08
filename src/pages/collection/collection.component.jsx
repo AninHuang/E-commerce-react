0aff5415537be6b1603749e38dbfc896174905a4
AninHuang <lifeanne19904@gmail.com>
2020-06-08 20:48:10 +0800

    Debug

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -8,10 +8,7 @@ import { selectCollection } from "../../redux/shop/shop.selectors";
 import "./collection.styles.scss";
 
 const CollectionPage = ({ collection }) => {
-  // console.log(match); // { path: "/shop/:collectionId", url: "/shop/hats", isExact: true, params: { collectionId: "hats" } }
-  // Then we can use match.params.collectionId
   const { title, items } = collection;
-
   return (
     <div className="collection-page">
       <h2 className="title">{title}</h2>
