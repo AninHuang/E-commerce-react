06b2d595c577289357167fe0dab432b671564258
AninHuang <lifeanne19904@gmail.com>
2020-08-23 11:26:12 +0800

    Using "as" prop to reuse styled component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -9,7 +9,6 @@ import {
   LogoContainer,
   OptionContainer,
   OptionLink,
-  OptionDiv,
 } from "./header.styles";
 
 import { auth } from "../../firebase/firebase.utils";
@@ -27,7 +26,9 @@ const Header = ({ currentUser, hidden }) => (
       <OptionLink to="/shop">SHOP</OptionLink>
       <OptionLink to="/shop">CONTACT</OptionLink>
       {currentUser ? (
-        <OptionDiv onClick={() => auth.signOut()}>SIGN OUT</OptionDiv>
+        <OptionLink as="div" onClick={() => auth.signOut()}>
+          SIGN OUT
+        </OptionLink>
       ) : (
         <OptionLink to="/signin">SIGN IN</OptionLink>
       )}
