2e82b5fc4ad9d799e2c676863f3fd2b06f3bd8ed
AninHuang <lifeanne19904@gmail.com>
2020-08-23 15:53:24 +0800

    Add a function in styled component to decide what styles should be rendered

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,22 +2,28 @@ import React from "react";
 
 import "./custom-buttom.styles.scss";
 
-const CustomButton = ({
-  children,
-  isGoogleSignIn,
-  inverted,
-  ...otherProps
-}) => (
-  // Always render class custom-button
-  <button
-    className={`${inverted ? "inverted" : ""} ${
-      isGoogleSignIn ? "google-sign-in" : ""
-    } custom-button`}
-    {...otherProps}
-  >
-    {children}
-    {/* children -> Get what's placed more code between CustomButton tag */}
-  </button>
+import { CustomButtonContainer } from "./custom-buttom.styles";
+
+const CustomButton = ({ children, ...props }) => (
+  <CustomButtonContainer {...props}>{children}</CustomButtonContainer>
 );
+
+// const CustomButton = ({
+//   children,
+//   isGoogleSignIn,
+//   inverted,
+//   ...otherProps
+// }) => (
+//   // Always render class custom-button
+//   <button
+//     className={`${inverted ? "inverted" : ""} ${
+//       isGoogleSignIn ? "google-sign-in" : ""
+//     } custom-button`}
+//     {...otherProps}
+//   >
+//     {children}
+//     {/* children -> Get what's placed more code between CustomButton tag */}
+//   </button>
+// );
 
 export default CustomButton;
