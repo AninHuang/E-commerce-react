2a703d396fa42e13dec5a1f18e60e618d4e63bf0
AninHuang <lifeanne19904@gmail.com>
2020-09-27 16:17:07 +0800

    Set up shop type, action, reducer

--- README.md
+++
@@ -1,2 +0,0 @@
-# E-commerce-react
-An E-commerce app with Redux, Hooks, GraphQL, ContextAPI, Stripe, Firebase
--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -1,11 +1,14 @@
 import SHOP_DATA from "./shop.data";
+import ShopActionTypes from "./shop.types";
 
 const INITIAL_STATE = {
-  collections: SHOP_DATA
+  collections: SHOP_DATA,
 };
 
 const shopReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case ShopActionTypes.UPDATE_COLLECTIONS:
+      return {};
     default:
       return state;
   }
